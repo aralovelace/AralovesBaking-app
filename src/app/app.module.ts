80489a879ff1013e4869a9d9f77a27e1329c303b
April Smith <aprilroseannkwong@gmail.com>
2020-07-11 17:56:45 +0100

    add dropdown directive
    toggle open and close for dropdowns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 import { FormsModule} from "@angular/forms";
+import {DropdownDirective} from "./shared/dropdown.directive";
 
 @NgModule({
     declarations: [
@@ -22,7 +23,8 @@ import { FormsModule} from "@angular/forms";
         RecipeDetailsComponent,
         RecipeItemComponent,
         ShoppingListComponent,
-        ShoppingEditComponent
+        ShoppingEditComponent,
+        DropdownDirective
     ],
   imports: [
     BrowserModule,
