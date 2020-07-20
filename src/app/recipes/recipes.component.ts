3569e2225def1c48bb25a89806324e5cc5d40eb9
April Smith <aprilroseannkwong@gmail.com>
2020-07-20 14:32:23 +0100

    Replace EventEmitter to Subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,23 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from "./recipe.model";
-import {RecipesService} from "./recipes.service";
+
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
-  providers: [RecipesService]
+  providers: []
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-  constructor(private recipesService: RecipesService) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.recipesService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe
-      }
-    );
-  }
+  ngOnInit(): void {}
 
 }
