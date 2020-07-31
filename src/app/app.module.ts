bea70c98b878ee1c1ce422b40a97f36a20ee664d
April Smith <aprilroseannkwong@gmail.com>
2020-07-31 09:44:20 +0100

    Dynamic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,13 @@ import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
-
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import {AuthComponent} from "./auth/auth.component";
 import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth.interceptor.service";
+import {AlertComponent} from "./shared/alert/alert.component";
+import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 
 
 @NgModule({
@@ -39,7 +39,9 @@ import {AuthInterceptorService} from "./auth/auth.interceptor.service";
         RecipeStartComponent,
         RecipeEditComponent,
         AuthComponent,
-        LoadingSpinnerComponent
+        LoadingSpinnerComponent,
+        AlertComponent,
+        PlaceholderDirective
     ],
   imports: [
     BrowserModule,
