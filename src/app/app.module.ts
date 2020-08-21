71d05f27aafab5b168fcc8dd10ccfa2deb24f22c
April Smith <aprilroseannkwong@gmail.com>
2020-08-21 13:50:32 +0100

    Cleaner Store (ngRx) add global store folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-
 import { HttpClientModule} from "@angular/common/http";
-
 import {StoreModule} from "@ngrx/store";
-
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
 import {LoggingService} from "./logging.service";
-import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
-
-
+import * as fromApp from'./store/app.reducer';
 
 @NgModule({
     declarations: [
@@ -26,7 +21,7 @@ import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducer";
     HttpClientModule,
     SharedModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(fromApp.appReducers)
   ],
 
   bootstrap: [AppComponent],
